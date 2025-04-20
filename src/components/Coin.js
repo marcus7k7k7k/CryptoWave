1fbb6c026c7f27e2e07714121313ae1715d25f37
MarcusC7k <marcus7k7k7k@gmail.com>
2025-04-20 19:05:19 +0800

    Update Market page: added coin list, search, and UI enhancements

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -2,28 +2,31 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Coin.css'
 
-const Coin = ({ coinID, image, name, symbol, price, volume, priceChange, marketcap }) => {
+const Coin = ({ coinID, rank, image, name, symbol, price, volume, priceChange, marketcap }) => {
   
     const navigate = useNavigate();
 
     // pass data to historypage
     const handleCoinClick = () => {
         navigate(`/history/${coinID}`, {
-            state:{coinID, image, name, symbol, price, volume, priceChange, marketcap}
+            state:{coinID, rank, image, name, symbol, price, volume, priceChange, marketcap}
         });
     }
   
     return (
         <div className='coin-container' onClick={handleCoinClick} style={{cursor:'pointer'}}>
             <div className="coin-row">
+                <div className="coin-rank">
+                    <p>{rank}</p>
+                </div>
                 <div className="coin">
                     <img src={image} alt="crypto" />
-                    <h1>{name}</h1>
+                    <h1 className="coin-name">{name}</h1>
                     <p className="coin-symbol">{symbol}</p>
                 </div>
                 <div className="coin-data">
-                    <p className="coin-price">${price}</p>
-                    <p className="coni-volume">${volume ?volume.toLocaleString() : 'N/A'}</p>
+                    <p className="coin-price">${price.toLocaleString()}</p>
+                    <p className="coin-volume">${volume ?volume.toLocaleString() : 'N/A'}</p>
                     { priceChange !== null && priceChange !== undefined ?
                     (
                         <p className={`coin-percent ${priceChange > 0 ? 'green' : 'red' }`}>
@@ -32,7 +35,7 @@ const Coin = ({ coinID, image, name, symbol, price, volume, priceChange, marketc
                     ):(
                         <p className="coin-percent">N/A</p>
                     )}
-                    <p className="coin-marketcap">Mkt Cap: ${marketcap.toLocaleString()}</p>
+                    <p className="coin-marketcap">${marketcap.toLocaleString()}</p>
                 </div>
             </div>
         </div>
--- src/components/Market_Price.js
+++ src/components/Market_Price.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './Market_Price.css';
 import Coin from './Coin';
+import './Coin.css';
 
 function Market_Price() {
 
@@ -12,7 +13,7 @@ function Market_Price() {
   useEffect(()=>{
     axios
     .get(
-      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1'
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1'
     )
     .then(res => {
       setCoins(res.data);
@@ -35,7 +36,7 @@ function Market_Price() {
   return (
     <div className="coin-app">
       <div className="coin-search">
-        <div className="coin-text">Search a currency</div>
+        <div className="coin-text">Search a Currency</div>
         <form>
           <input 
             type="text" 
@@ -44,12 +45,32 @@ function Market_Price() {
             onChange={handleSearchChange}
           />
         </form>
-
       </div>
+
+      <div className='coin-header'>
+        <div className="coin-row">
+          <div className="coin-rank">
+            <p>#</p>
+          </div> 
+          <div className="coin">
+            <p className="coin-space"></p>
+            <p className="coin-name">Coin</p>
+            <p className="coin-symbol">abbr.</p>
+          </div>
+          <div className="coin-data">
+            <p className="coin-price">Price</p>
+            <p className="coin-volume">Volume</p>
+            <p className="coin-percent">Price Change</p>
+            <p className="coin-marketcap">Market Cap</p>
+          </div>
+        </div>
+      </div>
+
       {filteredCoins.map(coin => {
         return <Coin
           key={coin.id}
           coinID={coin.id}
+          rank={coin.market_cap_rank}
           image={coin.image}
           name={coin.name}
           symbol={coin.symbol}
